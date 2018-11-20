f3e7201907fd790b3856f179efb651214f295861
artemis-beta <KXZ186@student.bham.ac.uk>
2018-11-20 23:29:29 +0000

    Updated requirements in setup

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(name                =  'HEPGen'                                      ,
       packages            =  ['hepgen']                                    ,
       zip_safe            =  False                                         ,
       include_package_data = True                                          ,
-      install_requires    =  []
+      install_requires    =  ['pypdt', 'matplotlib', 'scipy']
      )
