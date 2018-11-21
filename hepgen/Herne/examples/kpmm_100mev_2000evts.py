308ed4c0d089d11799aed3fd56afa4a280dd5e12
artemis-beta <KXZ186@student.bham.ac.uk>
2018-11-21 08:17:11 +0000

    Added Herne example

---
+++ hepgen/Herne/examples/kpmm_100mev_2000evts.py
@@ -0,0 +1,7 @@
+from hepgen.Herne.Configurables import Generator
+
+Generator().OutFile = 'KPMMMC.dtf'
+Generator().OutTree = 'KPiMuMuTree'
+Generator().DecID = 'SK020001'
+Generator().nEvts = 2000
+Generator().Energy = 100
