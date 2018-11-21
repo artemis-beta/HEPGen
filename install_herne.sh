4e65fb04bc1ac5e5a944ec935bc27bc529623b2c
artemis-beta <KXZ186@student.bham.ac.uk>
2018-11-21 00:08:48 +0000

    Removed unneeded installer

--- install_herne.sh
+++
@@ -1,5 +0,0 @@
-# Adds Herne compatibility for easy configuration
-
-pip install git+https://github.com/artemis-beta/Herne.git
-
-pip install --upgrade https://github.com/artemis-beta/archive/hepgen-herne.zip
