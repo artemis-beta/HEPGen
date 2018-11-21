24fc9257a904c4fb9072f8a7ef00f30929819c6a
artemis-beta <KXZ186@student.bham.ac.uk>
2018-11-21 00:23:44 +0000

    Switch branch instead

--- install_herne.sh
+++ install_herne.sh
@@ -1,5 +1,5 @@
 # Adds Herne compatibility for easy configuration
 
-pip install git+https://github.com/artemis-beta/Herne.git
+pip install $@ git+https://github.com/artemis-beta/Herne.git
 
-pip install --upgrade https://github.com/artemis-beta/archive/hepgen-herne.zip
+git checkout hepgen-herne
